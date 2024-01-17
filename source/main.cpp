e4a322eba2b9e08e0441f343ab7dfd8b6ad2583c
STILSIK99 <viktorpuh@gmail.com>
2024-01-18 01:40:36 +0400

    Initial

---
+++ source/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "First cmake's project in vs code\n";
+
+    int a = 10;
+    int c = a - 3;
+    a += c;
+    cout << c;
+    return 0;
+}
